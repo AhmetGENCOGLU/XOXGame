db2b078d7284b4f4028fccf121c8fd98b0d646e0
AhmetGENCOGLU <agncglu@gmail.com>
2022-05-06 22:36:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+  const defaultCells = ["", "", "", "", "", "","", "", ""];
+  const [cells, setCells] = useState(defaultCells);
+  const [isFirstUser, setIsFirstUser] = useState(true);
+  const [firstUserScore, setFirstUserScore] = useState(0);
+  const [secondUserScore, setSecondUserScore] = useState(0);
+
+  const isEmpty = (cell) => {
+    return cell === ""
+  };
+
+  const isSuccess = (cellss) => {
+    return (
+      (
+        (cellss[0] === cellss[1]) && (cellss[1] === cellss[2]) && !isEmpty(cellss[0])
+      ) ||
+      (
+        (cellss[3] === cellss[4]) && (cellss[4] === cellss[5]) && !isEmpty(cellss[3])
+      ) ||
+      (
+        (cellss[6] === cellss[7]) && (cellss[7] === cellss[8]) && !isEmpty(cellss[6])
+      ) ||
+      (
+        (cellss[0] === cellss[3]) && (cellss[3] === cellss[6]) && !isEmpty(cellss[0])
+      ) ||
+      (
+        (cellss[1] === cellss[4]) && (cellss[4] === cellss[7]) && !isEmpty(cellss[1])
+      ) ||
+      (
+        (cellss[2] === cellss[5]) && (cellss[5] === cellss[8]) && !isEmpty(cellss[2])
+      ) ||
+      (
+        (cellss[0] === cellss[4]) && (cellss[4] === cellss[8]) && !isEmpty(cellss[0])
+      ) ||
+      (
+        (cellss[2] === cellss[4]) && (cellss[4] === cellss[6]) && !isEmpty(cellss[2])
+      )
+    )
+  };
+
+  const onClickCell = (index) => {
+    if (isEmpty(cells[index])) {
+      let existCells = cells;
+      existCells[index] = isFirstUser ? "X" : "O";
+      setCells(existCells);
+      setIsFirstUser(!isFirstUser);
+      if (isSuccess(existCells) && isFirstUser) {
+        setFirstUserScore(firstUserScore + 1);
+        setCells(defaultCells);
+      } else if (isSuccess(existCells) && !isFirstUser) {
+        setSecondUserScore(secondUserScore + 1);
+        setCells(defaultCells);
+      }
+    } else {
+      alert("Cell isnt empty");
+    }
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='box firstUser'>
+        <div className={`title ${ isFirstUser && "active" }`}>User 1</div>
+        <div className='score'>
+          {firstUserScore}
+        </div>
+      </div>
+      <div className='box gameZone'>
+        {cells.map((child, index) => (
+          <div key={index} className='gameCell' onClick={() => onClickCell(index)}>
+            {child}
+          </div>
+        ))}
+      </div>
+      <div className='box secondUser'>
+        <div className={`title ${ !isFirstUser && "active" }`}> User 2</div>
+        <div className='score'>
+          {secondUserScore}
+        </div>
+      </div>
     </div>
   );
 }
